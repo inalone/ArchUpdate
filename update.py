4858a09353dec10c6a0c482c968d2462643239dd
inalone <myles.gordon@protonmail.com>
2020-04-01 12:47:39 +0100

    working on update feature

--- update.py
+++ update.py
@@ -1,2 +1,26 @@
+import subprocess
+
+def checkYay(user):
+    try:
+        subprocess.Popen(["sudo", "-u", user, "yay", "-V"])
+        return True
+    except:
+        return False
+
+def getUser():
+    proc = subprocess.Popen(["sudo", "sh", "-c", "'echo $SUDO_USER'"], shell=True, stdout=subprocess.PIPE)
+    return proc.communicate()
+
 def update():
-    pass
+    command = []
+
+    user = getUser()
+    print(user)
+
+    if(checkYay(user)):
+        command = ["sudo", "-u", user, "yay"]
+    else:
+        command = ["sudo", "pacman", "-Syu"]
+
+    proc = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
+    print(proc.stdout)
